807a8a3929fa97715a6d56db954e5f6df7b10952
dhanish2k <dhanish2k@gmail.com>
2015-04-29 13:55:54 +0000

    Updated View

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,18 +8,16 @@ class HomeController < ApplicationController
         @graph = Koala::Facebook::API.new(current_user.token)
         @places = @graph.get_connections("me", "tagged_places") 
         @places.each do |place|           
-           plc=Place.find_by uid: current_user.uid, lat: place["place"]["location"]["latitude"].to_s, lang: place["place"]["location"]["longitude"].to_s 
-           if plc.nil?
-              plc=Place.new()
-              plc.uid=current_user.uid
-              plc.name=place[   "place"]["name"]
-              plc.lat=place["place"]["location"]["latitude"]
-              plc.lang=place["place"]["location"]["longitude"] 
-              @instagram=HTTParty.get("https://api.instagram.com/v1/locations/search?lat=#{plc.lat}&lng=#{plc.lang}&client_id=62f331796f0549ca9eeece6831642f0f")
-              puts @instagram
-              plc.save!
+           @plc,@ter=Place.find_by uid: current_user.uid, lat: place["place"]["location"]["latitude"].to_s, lang: place["place"]["location"]["longitude"].to_s 
+           if @plc.nil?
+              @plc=Place.new()
+              @plc.uid=current_user.uid
+              @plc.name=place["place"]["name"]
+              @plc.lat=place["place"]["location"]["latitude"]
+              @plc.lang=place["place"]["location"]["longitude"] 
+              @plc.save!
            end
-        end
+         end
         @ps=Place.where("uid = ? ", current_user.uid)
         @hash = Gmaps4rails.build_markers(@ps) do |user, marker|
            marker.lat user.lat
@@ -31,6 +29,8 @@ class HomeController < ApplicationController
   end     
    
    def insta
-      
+      plc=Place.find_by_name(params[:pa])
+      @instagram=HTTParty.get("https://api.instagram.com/v1/locations/search?lat=#{plc.lat}&lng=#{plc.lang}&client_id="+ENV["CLIENT_ID"])
+      puts @instagram
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
    get 'home/index'
    get 'home/front'
+   get 'home/insta'
   # You can have the root of your site routed with "root"
    root 'home#front'
 
